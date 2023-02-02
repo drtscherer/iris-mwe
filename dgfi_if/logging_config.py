d99d6440691c2847a37594ede3b27f0500f890d0
Daniel <drtscherer@gmail.com>
2023-02-02 14:24:38 +0000

    minor

--- dgfi_if/logging_config.py
+++ dgfi_if/logging_config.py
@@ -4,17 +4,17 @@ from datetime import datetime
 log_date = datetime.strftime(datetime.now(),'%Y%m%d')
 
 
-if 'Logs' not in os.listdir('.'):
-    os.mkdir('Logs')
-if 'dgfi_if_logs' not in os.listdir('Logs/'):
-    os.mkdir('Logs/dgfi_if_logs')
+# if 'Logs' not in os.listdir('.'):
+#     os.mkdir('Logs')
+# if 'dgfi_if_logs' not in os.listdir('Logs/'):
+#     os.mkdir('Logs/dgfi_if_logs')
 
 
 logging.basicConfig(
     format="%(asctime)s %(name)s %(levelname)s : %(message)s",
     datefmt='%Y-%m-%d %H:%M:%S',
     handlers=[
-        logging.FileHandler(f"Logs/dgfi_if_logs/{log_date}.log"),
+        # logging.FileHandler(f"Logs/dgfi_if_logs/{log_date}.log"),
         logging.StreamHandler(sys.stdout)
     ]
 )
